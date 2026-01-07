722a01e3a0c9701387fd31f897d508b6402acafd
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-01-07 17:35:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from NEW version 🚀');
+  res.send('Hello from Watchtower!');
 });
 
 const PORT = 8080;
